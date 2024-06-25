1e7729b931a18d5b50a6ed8715077c4123c5f73b
Al-Ameen Babawale <60514061+Bee-code-tech@users.noreply.github.com>
2024-06-25 23:27:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const countdown = () => {
   const clock = document.querySelector('.countdown');
   const numbers = document.querySelectorAll('.txt');
-  const countDate = new Date("February 28, 2023 00:00:00").getTime();
+  const countDate = new Date("February 28, 2025 00:00:00").getTime();
   const currentTime = new Date().getTime();
   
   const gap = countDate - currentTime;
